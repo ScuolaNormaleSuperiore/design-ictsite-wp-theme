dcf03de44a194ed635e48070759c2ca7052c4df8
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-03-26 11:22:26 +0100

    New fetaure: Added Dockerfile

---
+++ DEV/Docker/configure/createUserAndDatabase.sql
@@ -0,0 +1,4 @@
+CREATE USER 'admin'@localhost IDENTIFIED BY 'admin';
+CREATE DATABASE  IF NOT EXISTS demoictdb CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ;
+GRANT ALL ON demoictdb.* TO 'admin'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
