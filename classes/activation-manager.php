01602a76d4f7b1b25e2c641791855c5dfffaed74
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-04-24 16:08:43 +0200

    mc

--- classes/activation-manager.php
+++ classes/activation-manager.php
@@ -25,16 +25,30 @@ class DIS_ActivationManager {
 	public function reload_data(){
 		error_log( '*** ACTION RELOAD DATA ***' );
 
-		
+		$this->pages_creation();
+		// $this->taxonomies_creation();
+		// $this->menu_creation();
+
 		// global $wp_rewrite;
 		// $wp_rewrite->init(); // important...
 		// $wp_rewrite->set_tag_base( 'argomento' );
 		// $wp_rewrite->flush_rules();
 
-
 		$this->result['status'] = 1;
 		$this->result['data']   = 'List of all the modifications';
 		return $this->result;
+	}
+
+	private function pages_creation() {
+		return true;
+	}
+
+	private function taxonomies_creation() {
+		return true;
+	}
+
+	private function menu_creation() {
+		return true;
 	}
 
 }
