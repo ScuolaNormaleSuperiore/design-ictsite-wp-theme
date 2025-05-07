d43be1f8dae5a69e9767fd953a28db4f27ab8297
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-05-07 15:37:49 +0200

    mc

--- template-parts/home/hp-featured-contents-section.php
+++ template-parts/home/hp-featured-contents-section.php
@@ -7,7 +7,53 @@
 
 ?>
 
-<section class="section pt-5 pb-3">
-	<H2>FEATURED CONTENTS</H2>
-</section>
+	<section id="blocco-servizi" class="section pt-5 pb-5">
+		<div class="section-content">
+			<div class="container">
+				<h2 class="visually-hidden">Servizi in evidenza</h2>
+				<div class="row">
+					<div class="card-wrapper card-teaser-wrapper card-teaser-block-3">
+
+
+						<!--start card-->
+						<div class="card card-bg rounded card-teaser bg-white" style="border-top: 3px solid">
+							<div class="card-body text-center">
+								<a href="schedaservizio.html">
+									<h3 class="card-title h3 text-primary">Green computing</h3>
+									<p class="card-text font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+										eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+								</a>
+							</div>
+						</div>
+						<!--end card -->
+						<!--start card-->
+						<div class="card card-bg rounded card-teaser bg-white" style="border-top: 3px solid">
+							<div class="card-body text-center">
+								<a href="schedaservizio.html">
+									<h3 class="card-title h3 text-primary">Green computing</h3>
+									<p class="card-text font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+										eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+								</a>
+							</div>
+						</div>
+						<!--end card -->
+						<!--start card-->
+						<div class="card card-bg rounded card-teaser bg-white" style="border-top: 3px solid">
+							<div class="card-body text-center">
+								<a href="schedaservizio.html">
+									<h3 class="card-title h3 text-primary">Green computing</h3>
+									<p class="card-text font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+										eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+								</a>
+							</div>
+						</div>
+						<!--end card -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
