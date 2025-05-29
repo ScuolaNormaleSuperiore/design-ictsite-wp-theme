a298da2b590f19a403979439ed18e284eeb87158
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-05-29 15:06:50 +0200

    mc

--- page-templates/people.php
+++ page-templates/people.php
@@ -6,6 +6,30 @@
 
 global $post;
 get_header();
+$value= 'slug';
+
+$current   = pll_current_language($value);
+$default   = pll_default_language($value);
+$languages = pll_languages_list();
+$string = 'Event';
+
+$current_locale = get_locale();
+
+$result   = switch_to_locale( 'it_IT' );
+$tradotto = _x( 'Event', 'DIS_PostTypeLabels', 'design_ict_site' );
+echo $tradotto;
+$result = restore_previous_locale();
+
+$result   =  switch_to_locale( 'en_US' );
+$tradotto = _x( 'Event', 'DIS_PostTypeLabels', 'design_ict_site' );
+echo $tradotto;
+$result = restore_previous_locale();
+
+$result   =  switch_to_locale( 'es_ES' );
+$tradotto = _x( 'Event', 'DIS_PostTypeLabels', 'design_ict_site' );
+echo $tradotto;
+$result = restore_previous_locale();
+
 
 ?>
 
