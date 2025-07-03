e84c7011c0c67499e6e813dd40a23d09ecb93557
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-07-03 12:06:02 +0200

    New feature: Added temporary list of services

--- page-templates/service-items.php
+++ page-templates/service-items.php
@@ -7,10 +7,26 @@
 global $post;
 get_header();
 
+$services = DIS_ContentsManager::get_service_list();
 ?>
 
 <div class="container shadow rounded  p-4 pt-3 pb-3 mb-5">
 	<h2 class="pb-2"><?php echo get_the_title(); ?></h2>
+
+	<ul>
+		<?php
+		foreach( $services as $service ) {
+		?>
+			<li>
+				<a href="<?php echo get_permalink( $service ); ?>">
+					<?php echo get_the_title( $service );?>
+				</a>
+			</li>
+		<?php
+		}
+		?>
+	</ul>
+
 </div>
 
 <?php
