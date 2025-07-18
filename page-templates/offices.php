2c27c9fb24156e4f186b5bb9bb136d07e01b3a29
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-07-18 11:48:15 +0200

    Added the template of the detail office page

--- page-templates/offices.php
+++ page-templates/offices.php
@@ -7,11 +7,20 @@
 global $post;
 get_header();
 
-
+$items = DIS_ContentsManager::get_generic_post_list( DIS_OFFICE_POST_TYPE );
 ?>
 
 <div class="container shadow rounded  p-4 pt-3 pb-3 mb-5">
-	<h2 class="pb-2"><?php echo get_the_title(); ?></h2>
+	<h2 class="pb-2"><?php echo esc_attr( get_the_title() ); ?></h2>
+		<ul>
+		<?php foreach ( $items as $item ) : ?>
+			<li>
+				<a href="<?php echo esc_url( get_permalink( $item ) ); ?>">
+					<?php echo esc_attr( get_the_title( $item ) ); ?>
+				</a>
+			</li>
+		<?php endforeach ?>
+	</ul>
 </div>
 
 <?php
--- single-dis-office.php
+++ single-dis-office.php
@@ -17,142 +17,182 @@ $places      = DIS_CustomFieldsManager::get_field( 'places', $post->ID );
 $full_places = $places ? implode( ', ', wp_list_pluck( $places, 'post_title' ) ) : '';
 ?>
 
-<div class="container shadow rounded  p-4 pt-3 pb-3 mb-5">
-	<h2 class="pb-2"><?php echo get_the_title(); ?></h2>
 
-	<!-- BODY -->
-	<div class="container shadow rounded p-4 mb-5 mt-2">
-		<div class="row">
-			<div class="col-12 col-md-10 offset-md-1 col-lg-7 offset-lg-1 m-auto">
-				<p class="lead">
-					<?php echo get_the_content() ?>
+<div class="container shadow rounded  p-4 pt-3 pb-3 mb-5">
+	<div class="row">
+		<!-- UFFICIO -->
+		<div class="col">
+			<h2 class="pb-2">Nome dell'ufficio (sigla)</h2>
+
+			<!-- DESCRIZIONE -->
+			<div class="row pb-3">
+				<h3 class="it-page-section h4 visually-hidden" id="descrizione">Descrizione</h3>
+
+				<p>Proin placerat ipsum massa, ac commodo velit tempor quis. In ante augue, sodales ac rhoncus in, ultricies a
+					neque. Morbi non semper felis, at lacinia nibh. Nam quis elit massa. Interdum et malesuada fames ac ante
+					ipsum primis in faucibus. Aliquam laoreet, diam quis blandit porttitor, leo erat semper sem, vel sagittis
+					dolor quam eu magna. Nunc feugiat pretium tempor. Nam eget augue quis tellus viverra malesuada vel ut quam.
+					Cras vehicula rutrum
+					vehicula. Suspendisse efficitur eget purus vitae convallis. Integer euismod pharetra lorem, non ullamcorper
+					lorem euismod vel. Orci varius natoque
+					penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 				</p>
+				<p>{Descrizione estesa} Proin placerat ipsum massa, ac commodo velit tempor quis. In ante augue, sodales ac
+					rhoncus in, ultricies a neque. Morbi non semper felis, at lacinia nibh. Nam quis elit massa. Interdum et
+					malesuada fames ac ante ipsum primis in faucibus. Aliquam laoreet, diam quis blandit porttitor, leo erat
+					semper sem, vel sagittis dolor quam eu magna. Nunc feugiat pretium tempor. Nam eget augue quis tellus
+					viverra malesuada vel ut quam. Cras vehicula rutrum
+					vehicula. Suspendisse efficitur eget purus vitae convallis. Integer euismod pharetra lorem, non ullamcorper
+					lorem euismod vel. Orci varius natoque
+					penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+				</p>
+			</div>
+			<!-- MEMBRI DEL GRUPPO -->
+			<div class="row">
+				<h3 class="h4 service-paragraph mt-3">
+					<i class="bi bi-person" style="font-size: 1.75rem;"></i> Persone
+				</h3>
+				<!-- trattandosi di una lista c'è da capire in quale ordine visualizzare, potrebbe anche andare bene in ordine alfabetico per cognome -->
+				<ul class="it-card-list row" aria-label="Risultati della ricerca: ">
+					<li class="col-12 col-md-6 col-lg-6 mb-3 mb-md-4">
+						<!--start it-card-->
+						<article
+							class="it-card it-card-profile it-card-height-full it-card-border-top it-card-border-top-secondary rounded shadow-sm border">
+							<div class="it-card-profile-header">
+								<div class="it-card-profile">
+									<h4 class="it-card-profile-name ">
+										<a href="scheda-persona.html">Nome e cognome</a>
+									</h4>
+									<p class="it-card-profile-role">Responsabile di area</p>
+								</div>
+								<div class="avatar size-xl">
+									<img src="https://randomuser.me/api/portraits/women/14.jpg" alt="Woman image">
+								</div>
+							</div>
+							<div class="it-card-body">
+								<dl class="it-card-description-list">
+									<div>
+										<dd><a href="scheda-ufficio.html">Area progetti e servizi IT</a></dd>
+									</div>
+								</dl>
+							</div>
+						</article>
+						<!--end it-card-->
+					</li>
+					<li class="col-12 col-md-6 col-lg-6 mb-3 mb-md-4">
+						<!--start it-card-->
+						<article
+							class="it-card it-card-profile it-card-height-full it-card-border-top it-card-border-top-secondary rounded shadow-sm border">
+							<div class="it-card-profile-header">
+								<div class="it-card-profile">
+									<h4 class="it-card-profile-name ">
+										Nome (no-link)
+									</h4>
+									<p class="it-card-profile-role">Staff</p>
+								</div>
+								<div class="avatar size-xl">
+									<img src="https://randomuser.me/api/portraits/women/15.jpg" alt="Woman image">
+								</div>
+							</div>
+							<div class="it-card-body">
+								<dl class="it-card-description-list">
+									<div>
+										<dd><a href="scheda-ufficio.html">Area progetti e servizi IT</a></dd>
+									</div>
+								</dl>
+							</div>
+						</article>
+						<!--end it-card-->
+					</li>
+
+				</ul>
+			</div>
+
+			<!-- PROGETTI -->
+			<div class="row">
+				<h3 class="h4 service-paragraph mt-3">
+					<i class="bi bi-clipboard-data" style="font-size: 1.75rem;"></i> Progetti
+				</h3>
+				<!-- lista dei progetti collegati al servizio -->
+				<ul class="it-card-list row" aria-label="Risultati della ricerca: ">
+					<li class="col-12 col-md-6 col-lg-6 mb-3 mb-md-4">
+						<!--start it-card-->
+						<article class="it-card it-card-height-full rounded shadow-sm border">
+							<!--card first child is the title (link)-->
+							<h3 class="it-card-title ">
+								<a href="scheda-progetto.html">Primo progetto</a>
+							</h3>
+							<!--card body content-->
+							<div class="it-card-body">
+								<p class="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione
+									in massimo tre o quattro righe, senza troncamento.</p>
+							</div>
+
+						</article>
+						<!--end it-card-->
+					</li>
+					<li class="col-12 col-md-6 col-lg-6 mb-3 mb-md-4">
+						<!--start it-card-->
+						<article class="it-card it-card-height-full rounded shadow-sm border">
+							<!--card first child is the title (link)-->
+							<h3 class="it-card-title ">
+								<a href="scheda-progetto.html">Secondo progetto</a>
+							</h3>
+							<!--card body content-->
+							<div class="it-card-body">
+								<p class="it-card-text">Questo è un testo breve che riassume il contenuto della pagina di destinazione
+									in massimo tre o quattro righe, senza troncamento.</p>
+							</div>
+
+						</article>
+						<!--end it-card-->
+					</li>
+
+				</ul>
+			</div>
+
+		</div>
+		<!-- SIDEBAR ELENCO -->
+		<div class="col-12 col-lg-4 col-md-5">
+			<div class="sidebar-wrapper it-line-left-side ps-4">
+
+				<div class="it-list-wrapper">
+					<ul class="it-list">
+						<li class="list-item">
+							<div class="it-right-zone">
+								<span class="text"><a href="scheda-luogo.html">Nome del luogo in relazione con link</a></span>
+								<svg class="icon">
+									<title>Indirizzo</title>
+									<use href="/bootstrap-italia/svg/sprites.svg#it-map-marker"></use>
+								</svg>
+							</div>
+						<li class="list-item">
+							<div class="it-right-zone">
+								<span class="text">050 509662</span>
+								<svg class="icon">
+									<title>Telefono</title>
+									<use href="/bootstrap-italia/svg/sprites.svg#it-telephone"></use>
+								</svg>
+							</div>
+						</li>
+						<li class="list-item">
+							<div class="it-right-zone">
+								<span class="text">mail@sns.it</span>
+								<svg class="icon">
+									<title>E-mail</title>
+									<use href="/bootstrap-italia/svg/sprites.svg#it-mail"></use>
+								</svg>
+							</div>
+						</li>
+
+					</ul>
+				</div>
+
 			</div>
 		</div>
 	</div>
 
-	<!-- CONTACTS -->
-	<div class="col-12 col-md-12 col-lg-12 mb-3 mb-md-4">
-		<h3 class="pb-2">
-			<?php echo __( 'Contacts', 'design_ict_site' ); ?>
-		</h3>
-		<article
-			class="it-card it-card-profile it-card-height-full it-card-border-top it-card-border-top-secondary rounded shadow-sm border">
-			<div class="it-card-body">
-				<dl class="it-card-description-list">
-					<div>
-						<dt>
-							<?php echo __( 'E-mail', 'design_ict_site' ); ?>:
-						</dt>
-						<dd>
-							<a href="mailto:<?php echo esc_attr( $email ); ?>"><?php echo esc_attr( $email ); ?></a>
-						</dd>
-					</div>
-					<div>
-						<dt>
-							<?php echo __( 'Telephone', 'design_ict_site' ); ?>:
-						</dt>
-						<dd>
-							<?php echo esc_attr( $phone ); ?>
-						</dd>
-					</div>
-						<div>
-						<dt>
-							<?php echo __( 'Site', 'design_ict_site' ); ?>:
-						</dt>
-						<dd>
-							<?php echo esc_attr( $full_places ); ?>
-						</dd>
-					</div>
-				</dl>
-			</div>
-		</article>
-	</div>
-	
-	<!-- PEOPLE-->
-	<div class="row">
-		<h3 class="pb-2">
-			<?php echo __( 'People', 'design_ict_site' ); ?>
-		</h3>
-		<ul class="it-card-list row" aria-label="<?php echo __( 'Search results', 'design_ict_site' ); ?>:">
-			<?php
-			foreach ( $persons as $p ) {
-				$name         = DIS_CustomFieldsManager::get_field( 'name', $p->ID );
-				$surname      = DIS_CustomFieldsManager::get_field( 'surname', $p->ID );
-				$honorific    = DIS_CustomFieldsManager::get_field( 'honorific', $p->ID );
-				$detail_link  = DIS_CustomFieldsManager::get_field( 'detail_link', $p->ID );
-				$website      = DIS_CustomFieldsManager::get_field( 'website', $p->ID );
-				$roles        = get_the_terms( $p->ID, DIS_PERSON_ROLE_TAXONOMY );
-				$photo        = DIS_ContentsManager::get_image_metadata( $p, 'thumbnail', '/assets/img/person.png' );
-				$offices      = DIS_ContentsManager::get_person_offices( $p );
-				$full_name    = trim( join ( ' ', array( $honorific, $name, $surname ) ) );
-				$full_roles   = $roles ? implode(', ', wp_list_pluck( $roles, 'name' ) ) : '';
-				$full_offices = $offices ? implode(', ', wp_list_pluck( $offices, 'post_title') ) : '';
-				$attachment   = DIS_CustomFieldsManager::get_field( 'attachment_1', $p->ID );
-				$target       = ( $detail_link === 'detail_page' ) ? '_self' : '_blank';
-				if ( $detail_link === 'detail_page' ) {
-					$full_link = get_permalink( $p );
-				} else if ( $detail_link === 'attachment_1' ){
-					$full_link = $attachment['url'] ?? '';
-				} else {
-					$full_link = $website ?? '';
-				}
-			?>
-				<li class="col-12 col-md-6 col-lg-4 mb-3 mb-md-4">
-					<article class="it-card it-card-profile it-card-height-full it-card-border-top it-card-border-top-secondary rounded shadow-sm border">
-						<div class="it-card-profile-header">
-							<div class="it-card-profile">
-								<h4 class="it-card-profile-name ">
-									<?php
-									if ( $detail_link !== 'no_link' ) {
-									?>
-									<a
-										target="<?php echo $target; ?>"
-										href="<?php echo $full_link; ?>"
-									>
-									<?php
-									}
-									?>
-										<?php echo $full_name; ?>
-									<?php
-									if ( $detail_link !== 'no_link' ) {
-									?>
-									</a>
-									<?php
-									}
-									?>
-								</h4>
-								<p class="it-card-profile-role">
-									<?php echo $full_roles; ?>
-								</p>
-							</div>
-							<div class="avatar size-xl">
-								<img
-									src="<?php echo esc_url( $photo['image_url'] ); ?>"
-									title="<?php echo $full_name; ?>"
-									alt="<?php echo $full_name; ?>"
-								>
-							</div>
-						</div>
-						<div class="it-card-body">
-							<dl class="it-card-description-list">
-								<div>
-									<dd>
-										<?php echo $full_offices; ?>
-									</dd>
-								</div>
-							</dl>
-						</div>
-					</article>
-				</li>
-			<?php
-			}
-			?>
-
-		</ul>
-	</div>
 </div>
-
 
 <?php
 get_footer();
