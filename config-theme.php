872ba42e99d1042f93c933880727f771099e14cf
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-08-04 14:16:29 +0200

    mc

--- config-theme.php
+++ config-theme.php
@@ -273,7 +273,7 @@ define( 'DIS_ACF_SHORT_DESC_LENGTH', 30 );
 define( 'DIS_ACF_SHORT_TEXT_LENGTH', 256 );
 
 // PAGINATION constants.
-define( 'DIS_ITEMS_PER_PAGE', 4 );
+define( 'DIS_ITEMS_PER_PAGE', 10 );
 define(
 	'DIS_ITEMS_PER_PAGE_VALUES',
 	array( '4', '10', '20', '30', '40', '50' )
