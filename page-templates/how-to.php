08c639de989f803f83ebd1c9dfa2f68726a0e8bb
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2026-05-07 15:23:54 +0200

    Label translation

--- page-templates/how-to.php
+++ page-templates/how-to.php
@@ -9,9 +9,9 @@ get_header();
 ?>
 
 <div class="container shadow rounded  p-4 pt-3 pb-3 mb-5">
-	<h2 class="pb-2"><?php echo esc_html( get_the_title() ); ?></h2>
-
-
+	<h2 class="pb-2">
+		<?php echo esc_html( get_the_title() ); ?>
+	</h2>
 </div>
 
 
